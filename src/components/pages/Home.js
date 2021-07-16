5482edd23fc715bbbe3dd2328c27ee18a49e9856
Carlos Bessa <carlbessa1618@gmail.com>
2021-07-16 19:10:38 +0100

    updateColorsButton

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -10,7 +10,7 @@ import Footer from './Footer';
      return (
          <>
             <HeroSection />
-            {/* <Cards /> */}
+            <Cards />
              <Contacts /> 
             <Footer />
          </>
