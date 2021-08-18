b4aef5bb36722bd491ac86676e5a099445f8bf0e
Carlos Bessa <carlbessa1618@gmail.com>
2021-08-18 22:34:23 +0100

    navmenuactiveColor

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,25 +1,14 @@
 import React from 'react';
-import Avatar from '@material-ui/core/Avatar';
 import Button from '@material-ui/core/Button';
 import CssBaseline from '@material-ui/core/CssBaseline';
-import TextField from '@material-ui/core/TextField';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Checkbox from '@material-ui/core/Checkbox';
-import Link from '@material-ui/core/Link';
-import Grid from '@material-ui/core/Grid';
-import Box from '@material-ui/core/Box';
-import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
 import InputLabel from '@material-ui/core/InputLabel';
 import MenuItem from '@material-ui/core/MenuItem';
-import FormHelperText from '@material-ui/core/FormHelperText';
 import FormControl from '@material-ui/core/FormControl';
 import Select from '@material-ui/core/Select';
-import Checkout from './Checkout';
-import { useHistory } from 'react-router-dom';
-import App from '../App'
+
 
 const useStyles = makeStyles((theme) => ({
   paper: {
@@ -46,6 +35,7 @@ const useStyles = makeStyles((theme) => ({
   selectEmpty: {
     marginTop: theme.spacing(2),
   },
+  
 }));
 
 export default function SignUp() {
@@ -65,15 +55,12 @@ export default function SignUp() {
     <Container component="main" maxWidth="xs">
       <CssBaseline />
       <div className={classes.paper}>
-        {/* <Avatar className={classes.avatar}>
-          <LockOutlinedIcon />
-        </Avatar> */}
         <Typography component="h1" variant="h5">
           Personalizar Sabonete
         </Typography>
         
         <FormControl variant="outlined" className={classes.form}>
-        <InputLabel id="demo-simple-select-outlined-label">Fragância</InputLabel>
+        <InputLabel >Fragância</InputLabel>
         <Select
           labelId="demo-simple-select-outlined-label"
           id="demo-simple-select-outlined"
@@ -93,7 +80,7 @@ export default function SignUp() {
       
       <FormControl variant="outlined" className={classes.form}>
         <InputLabel id="demo-simple-select-outlined-label">Base</InputLabel>
-        <SelectField
+        <Select
           labelId="demo-simple-select-outlined-label"
           id="demo-simple-select-outlined"
           value={base}
@@ -106,54 +93,18 @@ export default function SignUp() {
           </MenuItem>
           <MenuItem value={10}>Leite de Cabra</MenuItem>
          
-        </SelectField>
+        </Select>
       </FormControl>
 
-
-        <form className={classes.form} noValidate>
-          
-           
-            
-            <Grid item xs={12}>
-              <FormControlLabel
-                control={<Checkbox value="allowExtraEmails" color="primary" />}
-                label="Desejo personalizar o sabonete?"
-              />
-            </Grid>
-            <Grid item xs={12} sm={6}>
-              <StyledTextField className={classes.form}
-                autoComplete="fname"
-                name="Nome do Bebé"
-                variant="outlined"
-                fullWidth
-                id="NomeDoBeBe"
-                label="Nome do Bebé"
-                autoFocus
-              />
-            </Grid>
-            
-          
           <Button
+          className={classes.submit}
             type="submit"
             fullWidth
             variant="contained"
             color="primary"
-            className={classes.submit}
-          >
-            Personalizar
-          </Button>
-          <Grid container justifyContent="flex-end">
-            {/* <Grid item>
-              <Link href="#" variant="body2">
-                Already have an account? Sign in
-              </Link>
-            </Grid> */}
-          </Grid>
-        </form>
+          >Personalizar</Button>   
       </div>
-      {/* <Box mt={5}>
-         <Copyright /> 
-      </Box> */}
-    </Container>
+     </Container>
+  
   );
 }
