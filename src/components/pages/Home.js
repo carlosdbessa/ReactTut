829ddbb1c0987ac04b9d9b05f1d41a943d14c5a6
carlosdbessa <carlbessa1618@gmail.com>
2021-08-26 09:15:13 +0100

    Update Home.js

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -10,6 +10,7 @@ import Footer from './Footer';
      return (
          <>
             <HeroSection />
+            <PricingTables />
             <Cards />
              <Contacts /> 
             <Footer />
