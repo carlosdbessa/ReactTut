9da6be3cfaee96e916fb6978a8eeb028cfe2be60
Carlos Bessa <carlbessa1618@gmail.com>
2022-06-03 20:04:06 +0100

    navbar2

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -40,10 +40,10 @@ function Navbar() {
                     </Link> 
                 
                    
-                    <div className = 'menu-icon' onClick = {handleClick}>
+                   {/*  <div className = 'menu-icon' onClick = {handleClick}>
                         <i className= { click ? 'fas fa-times' : 'fas fa-bars'} />
-                    </div>
-                    <ul className= {click ? 'nav-menu active' : 'nav-menu'}>
+                    </div> */}
+                    {/* <ul className= {click ? 'nav-menu active' : 'nav-menu'}> */}
                        {/*  <li className='nav-item'>
                             <Link to='/' className='nav-links' onClick={closeMobileMenu}>
                                 INÍCIO
@@ -64,7 +64,7 @@ function Navbar() {
                                SignUp
                             </Link>
                         </li> */}
-                    </ul>   
+                     {/* </ul>    */}
                     
              </div>
             </nav>
